84c437237ded518561e4dbf63d8b7a73b1cf3f69
Jader Cerqueira <jader58@live.com>
2026-08-09 12:30:11 -0300

    Setup estrutura Maven school-pickup-system (esqueleto hexagonal)

---
+++ src/main/java/com/schoolqueue/SchoolQueueApplication.java
@@ -0,0 +1,12 @@
+package com.schoolqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolQueueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolQueueApplication.class, args);
+    }
+}
